3443da25bf7ba13a42da349e209d381e1755fc79
jmg-joe <gagliano.joe1@gmail.com>
2019-12-18 23:44:31 -0600

    fix things

--- scripts.js
+++ scripts.js
@@ -1,19 +1,17 @@
 $(document).ready(() => {
-    var argg = ['so it looks like you are in charge of the music...', 'click one more time to access all the songs you need for a good time'];
+    var argg = ['click me again', 'click me one more time'];
     var i = 0;
     $('.selected-element').addClass('animate');
     $('.selected-element').click(() => {
         hello(argg, i)
         i++;
     });
-
 });
 
 
 function hello(arr, num) {
     if (num == 2) {
-        // var large = '<button class="btn btn-primary" onClick = "document.getElementById(\'a\').play()" > Play</button> <button class="btn btn-primary" onClick="document.getElementById(\'a\').pause()">Pause</button> <br><button class="btn btn-primary" onClick="document.getElementById(\'b\').play()">Play</button><button class="btn btn-primary" onClick="document.getElementById(\'b\').pause()">Pause</button><br /><button class="btn btn-primary" onClick="document.getElementById(\'c\').play()">Play</button><button class="btn btn-primary" onClick="document.getElementById(\'c\').pause()">Pause</button><audio id="a"><source src="assets/a.mp3" type="audio/mp3" /></audio><audio id="b"><source src="assets/b.mp3" type="audio/mp3" /></audio><audio id="c"><source src="assets/c.mp3" type="audio/mp3" /></audio>';
-        var large = '<div class="container-fluid four-loko"> <div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h3>Please Pick a Song that Matches the Mood</h3></div><div class="modal-body"><div class="col-xs-3 col-xs-offset-5"><div id="loadbar" style="display: none;"><div class="blockG" id="rotateG_01"></div><div class="blockG" id="rotateG_02"></div><div class="blockG" id="rotateG_03"></div><div class="blockG" id="rotateG_04"></div><div class="blockG" id="rotateG_05"></div><div class="blockG" id="rotateG_06"></div><div class="blockG" id="rotateG_07"></div><div class="blockG" id="rotateG_08"></div></div></div><div class="quiz" id="quiz" data-toggle="buttons"><label class="element-animation1 btn btn-lg btn-primary btn-block"><span class="btn-label"><iclass="glyphicon glyphicon-chevron-right"></i></span> <input onClick="test(\'a\')" type="radio" name="q_answer" value="1">Classic Party</label><label class="element-animation2 btn btn-lg btn-primary btn-block"><span class="btn-label"><iclass="glyphicon glyphicon-chevron-right"></i></span> <input onClick="test(\'b\')" type="radio" name="q_answer" value="2">Classic Party with a twist</label><label class="element-animation3 btn btn-lg btn-primary btn-block"><span class="btn-label"><iclass="glyphicon glyphicon-chevron-right"></i></span> <input onClick="test(\'c\')" type="radio" name="q_answer" value="3">South of the Border</label><label class="element-animation4 btn btn-lg btn-primary btn-block"><span class="btn-label"><iclass="glyphicon glyphicon-chevron-right"></i></span> <input onClick="test(\'d\')" type="radio" name="q_answer" value="4">Break it Down</label></div></div><div class="modal-footer text-muted"><span id="answer"></span></div></div></div></div >'
+        var large = '<div class="container-fluid four-loko"> <div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h3>Please Pick a Song that Matches the Mood</h3></div><div class="modal-body"><div class="col-xs-3 col-xs-offset-5"><div id="loadbar" style="display: none;"><div class="blockG" id="rotateG_01"></div><div class="blockG" id="rotateG_02"></div><div class="blockG" id="rotateG_03"></div><div class="blockG" id="rotateG_04"></div><div class="blockG" id="rotateG_05"></div><div class="blockG" id="rotateG_06"></div><div class="blockG" id="rotateG_07"></div><div class="blockG" id="rotateG_08"></div></div></div><div class="quiz" id="quiz" data-toggle="buttons"><label class="element-animation1 btn btn-lg btn-primary btn-block"><span class="btn-label"><iclass="glyphicon glyphicon-chevron-right"></i></span> <input onClick="test(\'a\');" type="radio" name="q_answer" value="1">Classic Party</label><label class="element-animation2 btn btn-lg btn-primary btn-block"><span class="btn-label"><iclass="glyphicon glyphicon-chevron-right"></i></span> <input onClick="test(\'b\');changeColor()" type="radio" name="q_answer" value="2">Classic Party with a twist</label><label class="element-animation3 btn btn-lg btn-primary btn-block"><span class="btn-label"><iclass="glyphicon glyphicon-chevron-right"></i></span> <input onClick="test(\'c\')" type="radio" name="q_answer" value="3">South of the Border</label><label class="element-animation4 btn btn-lg btn-primary btn-block"><span class="btn-label"><iclass="glyphicon glyphicon-chevron-right"></i></span> <input onClick="test(\'d\')" type="radio" name="q_answer" value="4">Break it Down</label></div></div><div class="modal-footer text-muted"><span id="answer"></span></div></div></div></div >'
         $('.selected-element').addClass('hide');
         $('.wrapper').append(large)
     }
@@ -32,7 +30,7 @@ function test(str) {
     if ($(elementId).prop("paused") == true) {
         $(elementId).trigger("play");
         $('.four-loko').css("background-image", elementSource);
-        changeColor(elementId);
+        changeColor();
         $('body').css("background-color", 'rgb(255, 255, 255');
     }
     else {
@@ -49,11 +47,10 @@ var requestAnimationFrame = window.requestAnimationFrame ||
 
 var delay = 0;
 
-function changeColor(cssId) {
+function changeColor() {
     // change to setTimeout function
     delay++;
-    console.log($(cssId).prop("paused"));
-    if (delay > 15 && $(cssId).prop("paused") == false) {
+    if (delay > 35) {
         bodyElement.style.backgroundColor = getRandomColor();
         delay = 0;
     }
